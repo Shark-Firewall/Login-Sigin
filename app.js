334bf9cb74aef208673c7079f7c58bde0604f576
Rajnish Kumar <Kumarrajnish363.rk@gmail.com>
2022-06-19 11:49:40 +0530

    Database Connected!

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const connectDB = require("./db/connect");
+const app = express();
+
+//config dotenv file
+require("dotenv").config();
+
+//input-output are in json
+app.use(express.json());
+
+const start = async () => {
+  try {
+    await connectDB(process.env.db_link);
+    app.listen(process.env.PORT, () => {
+      console.log(`Server started at ${process.env.PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
